4ef5f515c9fde07e4e4d8b1d2d532f0d985837d4
Raqua41 <aurimas.varnelis@ktu.edu>
2021-03-29 21:31:44 +0300

    Add project files.

---
+++ PC_Part_picker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PC_Part_picker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
